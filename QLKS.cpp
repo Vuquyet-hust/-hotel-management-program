783fb6d1fd54178e42d73970d83a8334c036929c
Vũ Xuân Quyết <quyet.vx224120@sis.hust.edu.vn>
2024-07-21 09:17:40 +0700

    Add files via upload

---
+++ QLKS.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+
+#define max 100
+using namespace std;
+
+//Lop khach hang
+
+// lop phong
+
+//them phong
+
+//tim phong
+
+//hien thi phong
+
+//lop quan ly khach san
+
+// bao cao khach
+
+//check in
+
+// phong trong
+
+//tim khach hang
+
+//check out
+
+//quan ly phong
+
+// ham main
